5d769279977de35c6ab57f74c329ba441a5f6fed
alfredofo <fo.alfredo@gmail.com>
2016-11-23 14:32:43 +0100

    add sidescroller motion_character jump
    + bounce

--- src/javagg/GamePanel.java
+++ src/javagg/GamePanel.java
@@ -37,14 +37,14 @@ class GamePanel extends JPanel implements ActionListener {
     public int rz_x = 350;
     public int rz_y = 482;// 600x 615y
 
-//    static final int height = 10;
-//    static final int width = 10;
-//    public float vDelta;
-//    public float rbDelta;
-//    public float rbDegDelta;
-//    public float gDelta;
-//    public boolean bounce = false;
-//    public int yPos;
+    static final int height = 10;
+    static final int width = 10;
+    public float vDelta;
+    public float rbDelta;
+    public float rbDegDelta;
+    public float gDelta;
+    public boolean bounce = false;
+    public int yPos;
 
     static int direction = 0;
 
@@ -72,11 +72,11 @@ class GamePanel extends JPanel implements ActionListener {
         this.addMouseListener(new GamePanel.GiocaListener());
         this.addMouseMotionListener(new GamePanel.GiocaListener());
 
-//        yPos = getPreferredSize().height - height ;
-//        vDelta = 0;
-//        gDelta = 0.25f;
-//        rbDegDelta = 2.25f;
-        
+        yPos = rz_y;
+        vDelta = 0;
+        gDelta = 0.25f;
+        rbDegDelta = 2.25f;
+
         setLayout(null);
 
         time = new Timer(15, this);
@@ -86,81 +86,39 @@ class GamePanel extends JPanel implements ActionListener {
             @Override
             public void keyPressed(KeyEvent kp) {
 
-//                if (kp.getKeyCode() == KeyEvent.VK_DOWN) {
-//                    direction = 1;
-//                }
                 if (kp.getKeyCode() == KeyEvent.VK_RIGHT) {
                     direction = 2;
                 }
                 if ((kp.getKeyCode() == KeyEvent.VK_LEFT)) {
                     direction = 3;
                 }
-//                if (kp.getKeyCode() == KeyEvent.VK_UP) {
-//                    direction = 4;
-//                }
+
                 if (kp.getKeyCode() == KeyEvent.VK_SPACE & rz_y <= 482) {
-                    if (jump == false) {
-                        jump = true;
-                        moveableDown = true;
-                        if (direction == 2) {
-                            jumpright = true;
-                        }
-                        if (direction == 3) {
-                            jumpright = false;
-                        }
-                    }
+
+                    vDelta = -8;
+                    rbDelta = vDelta;
+                    bounce = true;
+
                 }
             }
 
-//            @Override
-//            public void keyReleased(KeyEvent kr) {  // 0=still 1=up , 2=right , 3=left , 4=down
-//                if (direction == 1) {
-//                    obj = punto_dn;
-//                }
-//                if (direction == 2) {
-//                    obj = punto_dx;
-//                }
-//                if (direction == 3) {
-//                    obj = punto_sx;
-//                }
-//                if (direction == 4) {
-//                    obj = punto_up;
-//                }
-//
-//            }
         });
 
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) { // 0=still 1=up , 2=right , 3=left , 4=down
-//        if (direction == 1) {
-//            up();
-//        }
+    public void actionPerformed(ActionEvent e) {
+
         if (direction == 2) {
             right();
         }
         if (direction == 3) {
             left();
         }
-//        if (direction == 4) {
-//            down();
-//        }
+
         direction = 0;
     }
 
-    /*    @Override
-    public void paint(Graphics g) {
-        update(g);
-    }
-
-    @Override
-    public void update(Graphics g) {
-        g.drawImage(this.background_e, -270, 0, getParent());
-        // TODO switch background a seconda del livello .... scrollingside
-
-    }
-     */
     @Override
     public void paintComponent(Graphics g) {
 
@@ -171,61 +129,65 @@ class GamePanel extends JPanel implements ActionListener {
         setFocusable(true);
 
         setBackground(g2d);
-        // collisioni 
 
         Jump();
 
-        g2d.drawImage(obj, rz_x, rz_y, this);
+        g2d.drawImage(obj, rz_x, yPos, this);
         repaint();
     }
 
     void Jump() // Jump mechanism
     {
-
-        if (moveableDown == true) {
-            if (jump == true & rz_y >= 357) // max altezza jump
-            {
-
-                rz_y -= 8;
-                if (rz_y <= 357) {
-                    jump = false;
+        int height = getHeight();
+        // No point if we've not been sized...
+        if (height > 0) {
+            // Are we bouncing...
+            if (bounce) {
+                // Add the vDelta to the yPos
+                // vDelta may be postive or negative, allowing
+                // for both up and down movement...
+                yPos += vDelta;
+                // Add the gravity to the vDelta, this will slow down
+                // the upward movement and speed up the downward movement...
+                // You may wish to place a max speed to this
+                vDelta += gDelta;
+                // If the sprite is not on the ground...
+                if (yPos >= rz_y) { // + GamePanel.height >= height
+                    // Seat the sprite on the ground
+                    yPos = rz_y; // height - GamePanel.height
+                    // If the re-bound delta is 0 or more then we've stopped
+                    // bouncing...
+                    if (rbDelta >= 0) {
+                        // Stop bouncing...
+                        bounce = false;
+                    } else {
+                        // Add the re-bound degregation delta to the re-bound delta
+                        rbDelta += rbDegDelta;
+                        // Set the vDelta...
+                        vDelta = rbDelta;
+                    }
                 }
-            }
-            if (jump == false & rz_y < 482) // min altezza jump
-            {
-                rz_y += 8;
             }
         }
 
-    }// end up
+    }
 
     void left() {
         if (moveableLeft == true & bk_x > BKMIN_X + 250) {  // 250
-            bk_x -= 40; // decrease xcoord while moving left 8
+            bk_x -= 40;
 
         }// end if
     }
 
     void right() {
         if (moveableRight == true & bk_x < BKMAX_X - 900) { //900
-            bk_x += 40; // increasing xcoord while moving right 8
+            bk_x += 40;
 
-        }// end if
+        }
     }
 
-//    void up() {
-//        if (moveableUp == true) {
-//            rz_y += 8;
-//        }
-//    }
-//
-//    void down() {
-//        if (moveableDown == true) {
-//            rz_y -= 8;
-//        }
-//    }
     void setBackground(Graphics g2d) {
-        g2d.drawImage(background_e, 150 - bk_x, 0, null);
+        g2d.drawImage(background_e, 200 - bk_x, 0, null);
     }
 
     public class GiocaListener extends MouseAdapter {
